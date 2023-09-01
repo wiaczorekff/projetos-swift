f369f07795683b2ef7cfc30fab4b0674caf7166d
Felipe Wiaczorek <125615804+wiaczorekff@users.noreply.github.com>
2023-09-01 08:50:26 -0300

    primeira atividade
    adição de texto e imagem ao corpo do swiftUI

---
+++ Projeto1/Projeto1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Projeto1
+//
+//  Created by Student04 on 31/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Projeto1/Projeto1/Projeto1App.swift
@@ -0,0 +1,17 @@
+//
+//  Projeto1App.swift
+//  Projeto1
+//
+//  Created by Student04 on 31/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Projeto1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
